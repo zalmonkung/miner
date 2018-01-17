3892d3efe6a3b9f133b54f669c1060165d0c043c
zalmonkung <s.somprasong@hotmail.com>
2018-01-18 01:18:05 +0700

    Update and rename ehtminer.js to ethminer.js

---
+++ ethminer.js
@@ -0,0 +1,93 @@
+const https = require("https");
+var app = require('express')();
+var port = process.env.PORT || 7777;
+var token = 'B3s44sxg9DAHBtvktVIxtkCGbwFuNsMiT1f8zdRan09';
+var unpaid = 0.04325;
+var eththb = 0;
+var value = 0;
+
+
+function sendLine(msg){
+  const request = require('request');
+   request({
+     method: 'POST',
+     uri: 'https://notify-api.line.me/api/notify',
+     headers: {
+       'Content-Type': 'application/x-www-form-urlencoded',
+  },
+     'auth': {
+       'bearer': token
+  },form: {
+       message: msg,
+    }
+  }, (err,httpResponse,body) => {
+  });
+   console.log("sendLine : "+msg);
+}
+
+
+function getBx(){
+  const url ="https://bx.in.th/api/orderbook/?pairing=21";
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+
+  res.on("end", () => {
+    body = JSON.parse(body);
+    eththb = body.bids[0][0];
+  });
+});
+
+}
+
+async function getMiner(){
+	await getBx();
+  const url ="https://api.ethermine.org/miner/0x2c05c58775E35b1C576bC5F5Ec593b2e87587817/currentStats";
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+
+  res.on("end", () => {
+    body = JSON.parse(body);
+    unpaid = (body.data.unpaid*0.000000000000000001);
+    value = (unpaid*eththb).toFixed(2);
+    sendLine("ตอนนี้ได้เงินจากเครื่องขุดบิทคอยน์ "+value+" บาท");
+    
+  });
+});
+
+}
+
+//getMiner();
+
+
+
+
+app.get('/', function (req, res) {
+	getMiner();
+    res.send('<h1>'+"ตอนนี้ได้เงินจากเครื่องขุดบิทคอยน์ "+value+" บาท"+'</h1>');
+});
+
+app.listen(port, function() {
+    console.log('Starting node.js on port ' + port);
+});
+
+//getMiner();
+//setInterval(function(){getMiner()},10000);
+
+
+
+
+
+
+
+
+
+
+
